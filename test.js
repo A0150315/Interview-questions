662af20fcf17f587df6fd0376969fc782455d7bb
JasonTanMix <574469551@qq.com>
2017-11-18 19:48:05 +0800

    Signed-off-by: JasonTanMix <574469551@qq.com>

--- test.js
+++ test.js
@@ -1,20 +1,44 @@
-const promise = new Promise((resovle, reject) => {
-    resovle('success1');
-    reject('error');
-    resovle('success2');
-});
+function searchTwitter(term, onload, onerror) {
 
-promise.then(res => {
-    console.log('then: ', res);
-}).catch(err => {
-    console.log('catch: ', err);
-});
+     var xhr, results, url;
+     url = 'http://search.twitter.com/search.json?rpp=100&q=' + term;
+     xhr = new XMLHttpRequest();
+     xhr.open('GET', url, true);
 
-Promise.resovle(1).then(res => {
-    console.log(res);
-    return 2;
-}).catch(err => {
-    return 3;
-}).catch(res => {
-    console.log(res);
-});
+     xhr.onload = function (e) {
+         if (this.status === 200) {
+             results = JSON.parse(this.responseText);
+             onload(results);
+         }
+     };
+
+     xhr.onerror = function (e) {
+         onerror(e);
+     };
+
+     xhr.send();
+ }
+
+ function handleError(error) {
+     /* handle the error */
+ }
+
+ function concatResults() {
+     /* order tweets by date */
+ }
+
+ function loadTweets() {
+     var container = document.getElementById('container');
+
+     searchTwitter('#IE10', function (data1) {
+         searchTwitter('#IE9', function (data2) {
+             /* Reshuffle due to date */
+             var totalResults = concatResults(data1.results, data2.results);
+             totalResults.forEach(function (tweet) {
+                 var el = document.createElement('li');
+                 el.innerText = tweet.text;
+                 container.appendChild(el);
+             });
+         }, handleError);
+     }, handleError);
+ }
